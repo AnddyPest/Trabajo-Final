0391db3c132c300f1827f5d780679d7b8486dd99
Máximo quiroga <maxiquiroga2121@gmail.com>
2024-10-14 16:39:20 -0300

    Update PacienteData.java

--- Trabajo Final G11/src/Persistencia/PacienteData.java
+++ Trabajo Final G11/src/Persistencia/PacienteData.java
@@ -1,6 +1,40 @@
 package Persistencia;
 
+import java.sql.Connection;
+import Entidades.Paciente;
+
 
 public class PacienteData {
+    
+    Connection conexion;
+    
+    public PacienteData(Connection conexion){
+        this.conexion = conexion;
+    }
+    
+   // C - R - U - D 
+    
+    //Create
+    //crear Paciente
+
+    public void crearPaciente(Paciente pacienteEnviado){
+        
+        String query = "Insert into paciente( nombre, dni, domicilio, telefono ) values( ? , ? , ? , ? )";
+        
+        
+    }
+    
+    //READ
+    //Listar Todos Los Pacientes
+    //Listar Paciente por id
+    //Listar Paciente por nombre
+    
+    //UPDATE
+    //actualizar paciente por id
+    
+    //DELETE
+    //borrar paciente por id
+       
+    
     
 }
