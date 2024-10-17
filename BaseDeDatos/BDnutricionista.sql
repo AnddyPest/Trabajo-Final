03613a294de365558be9678731909d5555d483eb
Máximo quiroga <maxiquiroga2121@gmail.com>
2024-10-17 03:25:53 -0300

    Update BDnutricionista.sql

--- BaseDeDatos/BDnutricionista.sql
+++ BaseDeDatos/BDnutricionista.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-10-2024 a las 07:03:26
+-- Tiempo de generación: 17-10-2024 a las 08:25:40
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
