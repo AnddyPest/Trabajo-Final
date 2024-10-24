8d89fa2f87437fa684091d8667e9732e336613cf
Máximo quiroga <maxiquiroga2121@gmail.com>
2024-10-24 20:37:02 -0300

    Update TestClass.java

--- Trabajo Final G11/src/VistasInicio/TestClass.java
+++ Trabajo Final G11/src/VistasInicio/TestClass.java
@@ -19,12 +19,11 @@ public class TestClass {
         ////////////////////////////
         
         PacienteData pacienteData = new PacienteData(conexion);
-//        
-//        Paciente paciente1 = new Paciente("maximo", 44332211, "b 123 cc122", "2552331", true);
-//        Paciente paciente2 = new Paciente("ramon", 46532211, "b 126 cc122", "9952331", true);
-//        Paciente paciente3 = new Paciente("andres", 41332211, "b 121 cc122", "2752331", true);
-//        Paciente paciente4 = new Paciente("marquitos", 31332211, "b 333 cc122", "232331", true);
-//        Paciente paciente5 = new Paciente("maximo", 443322211, "b 1 cc122", "2552331", true);
+
+        Paciente paciente1 = new Paciente("maximo", 44, 21, "446503175",75, 78);
+        Paciente paciente2 = new Paciente("ramon", 33, 25, "33411",75, 79);
+        Paciente paciente3 = new Paciente("tomas", 45, 23, "4465033",72, 71);
+        Paciente paciente4 = new Paciente("maximoasdsd", 4667, 11, "546503175",65, 78);
        
         //Crear || FUNCIONA CORRECTAMENTE ||   Nota Hay que VALIDAR el telefono, para saber si es un numero y no contiene letras
 //        
@@ -43,14 +42,14 @@ public class TestClass {
 
         //buscar Paciente por Id || FUNCIONA CORRECTAMENTE || NOTA: Puede que haya que validar null al recibir el dato
         
-//        Paciente pacienteEncontrado = pacienteData.buscarPacientePorID(2);
+//        Paciente pacienteEncontrado = pacienteData.buscarPacientePorID(12);
 //        if(!(pacienteEncontrado == null)){
 //            System.out.println(pacienteEncontrado.toString());
 //        }       
         
         //Buscar paciente por nombre || FUNCIONA CORRECTAMENTE || NOTA: Mismo caso que el anterior, validar si llega null
         
-//        ArrayList<Paciente> pacientesRecibidos = pacienteData.buscarPacientesPorNombre(" maximo");
+//        ArrayList<Paciente> pacientesRecibidos = pacienteData.buscarPacientesPorNombre(" maximo sd");
 //        if(!pacientesRecibidos.isEmpty()){
 //            for(Paciente paciente: pacientesRecibidos){
 //                System.out.println(paciente.toString());
@@ -59,17 +58,17 @@ public class TestClass {
         
         
         //Actualizar paciente por id || FUNCIONA CORRECTAMENTE || Nota: Hay que enviarle todos los datos nuevos y viejos, porque si no, los  datos viejos no enviados, los borra
-//        Paciente pacientePrueba = new Paciente();
-//        Paciente pacienteActualizado = new Paciente(2,"Ramon", 43532211, "Casa Termo", "2952331", true);
-//        pacientePrueba.setIdPaciente(2);
-//        pacientePrueba.setNombre("ramon");
-//        pacientePrueba.setDni(461132111);
-//        pacientePrueba.setDomicilio("CasaLCOA");
-//        pacientePrueba.setTelefono("244232");
-//        pacientePrueba.setEstado(true);
-//        
-//        pacienteData.actualizarPacientePorId(pacientePrueba);
-//
+        Paciente pacientePrueba = new Paciente();
+        Paciente pacienteActualizado = new Paciente(2,"Ramon", 43532211, "Casa Termo", "2952331", true);
+        pacientePrueba.setIdPaciente(2);
+        pacientePrueba.setNombre("ramon");
+        pacientePrueba.setDni(461132111);
+        pacientePrueba.setDomicilio("CasaLCOA");
+        pacientePrueba.setTelefono("244232");
+        pacientePrueba.setEstado(true);
+        
+        pacienteData.actualizarPacientePorId(pacientePrueba);
+
 
         //Alta Logica FUNCIONA CORRECTAMENTE
       //  pacienteData.altaLogicaPaciente(10);
